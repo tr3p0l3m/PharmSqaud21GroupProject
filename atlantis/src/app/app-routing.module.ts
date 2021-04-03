5886e7e392ebd9978b8497e5552e91084ed533bd
tr3p0l3m <matiamukasa@outlook.com>
2021-04-03 22:49:56 +0000

    added some files

--- atlantis/src/app/app-routing.module.ts
+++ atlantis/src/app/app-routing.module.ts
@@ -19,6 +19,14 @@ const routes: Routes = [
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
+  {
+    path: 'booking',
+    loadChildren: () => import('./booking/booking.module').then( m => m.BookingPageModule)
+  },
+  {
+    path: 'flights',
+    loadChildren: () => import('./flights/flights.module').then( m => m.FlightsPageModule)
+  },
 ];
 
 @NgModule({
