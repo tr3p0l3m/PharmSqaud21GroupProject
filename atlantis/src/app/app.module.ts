7a70b3dd51ac321cf6b778b6ce5fa73077bec324
WisdomFromAbove# <wisdom.kalu@ashesi.edu.gh>
2021-04-13 16:32:46 +0000

    modified the frontpage and added a navbar

--- atlantis/src/app/app.module.ts
+++ atlantis/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HTTP } from '@ionic-native/http/ngx';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP } from '@ionic-native/http/ngx';
+import { Component } from '@angular/core';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [HTTP, HttpClient,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, HTTP, HttpClient, Component],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
