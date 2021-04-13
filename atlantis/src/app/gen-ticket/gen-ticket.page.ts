ccd37cccd6214197ff97186407c565c7a36c5185
WisdomFromAbove# <wisdom.kalu@ashesi.edu.gh>
2021-04-13 22:31:35 +0000

    made api calls

--- atlantis/src/app/gen-ticket/gen-ticket.page.ts
+++ atlantis/src/app/gen-ticket/gen-ticket.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HTTP } from '@ionic-native/http/ngx';
+
 
 @Component({
   selector: 'app-gen-ticket',
@@ -6,11 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gen-ticket.page.scss'],
 })
 export class GenTicketPage implements OnInit {
-  tickets: Array<object>;
-  constructor() { }
+  
+  //tickets: Array<object>;
+  //public ID = 1;
+  public data;
+  public ticketID;
+  public items: any;
+ 
+  constructor( private route: ActivatedRoute, private http: HTTP ) { }
 
+  async getTicket(tk=10) {
+ 
+    try {
+      const url = 'http://thecode4allinitiative.org/atlantis/atlantis/getTicket.php?ID=' + tk;
+      const params = {};
+      const headers = {};
+
+      const response = await this.http.get(url, params, headers);
+
+      console.log(response.status);
+      console.log(JSON.parse(response.data)); // JSON data returned by server
+      this.items =  JSON.parse(response.data);
+      console.log(response.headers);
+
+    } catch (error) {
+      console.error(error.status);
+      console.error(error.error); // Error message as string
+      console.error(error.headers);
+    }
+  }
   ngOnInit() {
-    fetch('http://thecode4allinitiative.org/atlantis/atlantis/getTicket.php').then(response => response.json()).then(parsedData => this.tickets = parsedData);
+
+    this.getTicket();
+
+    //fetch('http://thecode4allinitiative.org/atlantis/atlantis/getTicket.php').then(response => response.json()).then(parsedData => this.tickets = parsedData);
+  
   }
 
 }
