05d6375896b13c3c92265fa0c9ab69bf8282c2de
djinnsend <nanaboakye1738@gmail.com>
2021-04-13 20:47:50 +0000

    Moved notification to gen-ticket

--- atlantis/src/app/booking/booking.page.ts
+++ atlantis/src/app/booking/booking.page.ts
@@ -1,4 +1,3 @@
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,30 +8,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BookingPage implements OnInit {
   clickSub: any;
-  constructor( private http: HttpClient, private LocalNotifications: LocalNotifications) { }
+  constructor( private http: HttpClient) { }
 
   runHttp() {
     this.http.get('http://thecode4allinitiative.org/atlantis/atlantis/getAllFlightData.php')
     .subscribe(data => {
       console.log();
     });
-    this.Ticket_Notif();
   }
 
-  Ticket_Notif(){
-    this.LocalNotifications.schedule({
-      id: 1,
-      text: 'Your flight has been succesfully booked.',
-      data: {secret: 'secret'},
-      silent: false,
-      group: 'Atlantis',
-      autoClear: true,
-      launch: true,
-      priority: 3,
-      vibrate: true,
-      foreground: true
-    });
-  }
 
   ngOnInit() {
   }
--- atlantis/src/app/gen-ticket/gen-ticket.page.ts
+++ atlantis/src/app/gen-ticket/gen-ticket.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 
 @Component({
   selector: 'app-gen-ticket',
@@ -7,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenTicketPage implements OnInit {
   tickets: Array<object>;
-  constructor() { }
+  constructor(private LocalNotifications: LocalNotifications) { }
+
+  Ticket_Notif(){
+    this.LocalNotifications.schedule({
+      id: 1,
+      text: 'Your flight has been succesfully booked.',
+      data: {secret: 'secret'},
+      silent: false,
+      group: 'Atlantis',
+      autoClear: true,
+      launch: true,
+      priority: 3,
+      vibrate: true,
+      foreground: true
+    });
+  }
+
 
   ngOnInit() {
     fetch('http://thecode4allinitiative.org/atlantis/atlantis/getTicket.php').then(response => response.json()).then(parsedData => this.tickets = parsedData);
+    this.Ticket_Notif();
   }
 
 }
